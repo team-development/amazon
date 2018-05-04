3e6240ed9c0c5a962c53f653cbfd2496b5349bc2
james-knott <33873365+james-knott@users.noreply.github.com>
2018-05-04 09:36:24 -0500

    added awscli

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ sudo yum update -y
 sudo yum install python-pip git tmux -y
 #pip install ansible --user
 sudo -u vagrant pip install ansible --user
-sudo pip install ansible --user
+sudo pip install ansible awscli --user
 sudo -u vagrant wget https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg
 echo "roles_path    = /vagrant/ansible" >> /home/vagrant/ansible.cfg
 export ANSIBLE_CONFIG=/home/vagrant/ansible.cfg
