9876259856ce4148229637ce04a02fcc4118361c
james-knott <33873365+james-knott@users.noreply.github.com>
2018-05-04 09:37:59 -0500

    testing playbook

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ sudo pip install ansible awscli --user
 sudo -u vagrant wget https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg
 echo "roles_path    = /vagrant/ansible" >> /home/vagrant/ansible.cfg
 export ANSIBLE_CONFIG=/home/vagrant/ansible.cfg
-#sudo -u vagrant /home/vagrant/.local/bin/ansible-playbook /vagrant/ansible/dev.yml
+sudo -u vagrant /home/vagrant/.local/bin/ansible-playbook /vagrant/ansible/dev.yml
 #git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sudo -u vagrant chmod u+x /home/vagrant/.vim_runtime/install_awesome_vimrc.sh
 sudo -u vagrant sh ~/.vim_runtime/install_awesome_vimrc.sh
