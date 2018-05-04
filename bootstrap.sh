b5a2e5e999cc7ec3468489b3151f47d76ccc7369
james-knott <33873365+james-knott@users.noreply.github.com>
2018-05-04 08:37:08 -0500

    updated ansible.cfg

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ sudo yum update -y
 sudo yum install python-pip git tmux -y
 #pip install ansible --user
 sudo -u vagrant pip install ansible --user
-wget https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg
-#mv ansible.cfg /home/vagrant/
+sudo -u vagrant wget https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg
+echo " roles_path    = /vagrant/ansible " >> ansible.cfg
 export ANSIBLE_CONFIG=/home/vagrant/ansible.cfg
