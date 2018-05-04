3612554a5bbde9b5a49eaa0af9b4db0596e22f6a
james-knott <33873365+james-knott@users.noreply.github.com>
2018-05-04 08:12:08 -0500

    testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ sudo yum update -y
 sudo yum install python-pip git tmux -y
 pip install ansible --user
 wget https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg
-mv ansible.cfg /home/vagrant/
+#mv ansible.cfg /home/vagrant/
 export ANSIBLE_CONFIG=/home/vagrant/ansible.cfg
